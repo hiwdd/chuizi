f1d7331b7bbbf4ee2d21d3e2d61ecdc3e9539ce1
ww <wwwwww@qq.com>
2019-07-24 08:23:08 +0800

    删库跑路

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './router/AppRouter';
-import './static/css/reset.scss';
-
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
